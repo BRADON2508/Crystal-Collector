0b834a79916debc66bf732781953154e80df4165
Brandon <mgsbigboss85@live.com>
2018-12-02 15:59:45 -0500

    added new elements to HTML, added javascript elements, added both ajax CDN and a copy of as a file in javascript folder just to be sure

---
+++ assets/javascript/game.js
@@ -0,0 +1,50 @@
+var randNum = Math.round(Math.exp(Math.random()*Math.log(100-0+1)));
+		console.log(randNum);
+
+		var userClick = "";
+
+		do {
+
+		$(document).ready(function() {
+
+		    $('#promptUser').on('click', function(e){
+				e.preventDefault;
+
+			userClick = $('#inUserClick').val();
+			console.log("User Score:" + userScore);
+					
+				  
+			if(userScore == randNum){
+			    console.log("user guess is correct");
+			    alert("That's right! Play again.");
+				
+			    }  
+				else{
+				console.log("user score is incorrect");
+				alert("Not quite. Try again.");
+					
+				}
+
+			});
+		});
+		}
+		while (userClick != randNum);			
+				
+
+			/*generate new random number each on button click */
+        $("#Diamond").on("click", function(){
+            var randNum = Math.round(Math.exp(Math.random()*Math.log(100-0+1)));
+			console.log(randNum); /*(return random number to console for cheating)*/            
+        })
+        $("#Spade").on("click", function(){
+            var randNum = Math.round(Math.exp(Math.random()*Math.log(100-0+1)));
+			console.log(randNum); /*(return random number to console for cheating)*/            
+        })
+        $("#Heart").on("click", function(){
+            var randNum = Math.round(Math.exp(Math.random()*Math.log(100-0+1)));
+			console.log(randNum); /*(return random number to console for cheating)*/            
+        })
+        $("#Club").on("click", function(){
+            var randNum = Math.round(Math.exp(Math.random()*Math.log(100-0+1)));
+			console.log(randNum); /*(return random number to console for cheating)*/            
+        })
